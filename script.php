b37a84b702d09864051ec3758326475d6a6360a9
steven smith <jedistev@gmail.com>
2017-10-17 15:08:08 +0100

    Add Feature on include page
    add Footer.php, Header.php, Script.php and nav.php

---
+++ script.php
@@ -0,0 +1,15 @@
+ <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/popper/popper.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="js/jquery.simplePagination.js"></script>
+
+    <!-- Contact Form JavaScript -->
+    <script src="js/jqBootstrapValidation.js"></script>
+    <script src="js/contact_me.js"></script>
+
+    <!-- Custom scripts for this template -->
+    <script src="js/freelancer.min.js"></script>
