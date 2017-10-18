f3280853479dd60f3170b91bb2364a1cc59c38f4
steven smith <jedistev@gmail.com>
2017-10-18 10:55:09 +0100

    Add new files and break up
    Add new Files which include file php, break up pages and no longer write in index pages

--- each_settlement.php
+++ each_settlement.php
@@ -1,20 +1,10 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "amazon";
-
-$conn = mysqli_connect($servername, $username, $password, $dbname) or die("Connection failed: " . mysqli_connect_error());
-/* check connection */
-if (mysqli_connect_errno()) {
-    printf("Connect failed: %s\n", mysqli_connect_error());
-    exit();
-}
+//sql files for calucated
+include ('mainSql.php');
   
 $limit = 15;  
 if (isset($_GET["page"])) { $page  = $_GET["page"]; } else { $page=1; };  
 $start_from = ($page-1) * $limit;  
-
 
 ?> 
 
